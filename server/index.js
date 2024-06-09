ed4bd620b0f7c52625fe6f25c4142605b6baf042
avi <avilinid035@gmail.com>
2024-06-09 22:38:37 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
+const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "http://localhost:5175",
         methods: ["GET", "POST"]
     },
 });
@@ -20,16 +20,15 @@ io.on("connection", (socket) => {
 
     socket.on("send-message", (message) => {
         console.log(message);
-        // Broadcast the received message to all the connected users
         io.emit("received-message", message);
     });
 
     socket.on("delete-message", (messageId) => {
-        // Broadcast the message ID to all connected users for deletion
         io.emit("message-deleted", messageId);
     });
 
     socket.on("disconnected", () => console.log("User disconnected"));
 });
 
-server.listen(5000, () => console.log("Server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
